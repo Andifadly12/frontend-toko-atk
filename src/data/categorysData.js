7a134726c4b2e48207f3d4929470d9fba2d57d57
Andifadly12 <andifadly2004@gmail.com>
2026-07-08 14:39:08 +0800

    keempat pembaharuan: mebuat categorydata

---
+++ src/data/categorysData.js
@@ -0,0 +1,41 @@
+const categorysData = [
+  {
+    name: "name",
+    label: "Nama Kategori",
+    type: "text",
+    placeholder: "Contoh: Alat Tulis",
+    required: true,
+  },
+  {
+    name: "description",
+    label: "Deskripsi",
+    type: "textarea",
+    placeholder: "Contoh: Pulpen, pensil, spidol",
+  },
+  {
+    name: "total_products",
+    label: "Jumlah Produk",
+    type: "number",
+    placeholder: "Contoh: 20",
+  },
+  {
+    name: "status",
+    label: "Status",
+    type: "select",
+    placeholder: "Pilih status",
+    colorType: "primary",
+    options: [
+      {
+        label: "Aktif",
+        value: "active",
+      },
+      {
+        label: "Tidak Aktif",
+        value: "inactive",
+      },
+    ],
+  },
+];
+
+
+export default categorysData;
