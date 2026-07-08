b6223edb4cc451e71a15ac978a243f3755bfece8
Andifadly12 <andifadly2004@gmail.com>
2026-07-08 14:05:09 +0800

    keempat pembaharuan: mebuat categorySchema

---
+++ src/utils/categorySchema.js
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+const categorySchema = z.object({
+  name: z.string().min(1, "Nama kategori wajib diisi"),
+
+  description: z.string().optional(),
+
+  total_products: z.coerce
+    .number()
+    .min(0, "Jumlah produk tidak boleh minus"),
+
+  status: z.enum(["active", "inactive"], {
+    message: "Status wajib dipilih",
+  }),
+});
+
+export default categorySchema;
