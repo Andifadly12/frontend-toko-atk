5afbd8c9aecae24e3db2154f66da8eacc5022a24
Andifadly12 <andifadly2004@gmail.com>
2026-07-09 16:38:51 +0800

    kelimah perbaharuan: mebuat useSearch

---
+++ src/hooks/useSearch.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const useSearch = (data = [], keys = []) => {
+  const [search, setSearch] = useState("");
+
+  const filteredData = data.filter((item) => {
+    return keys.some((key) =>
+      String(item[key] || "")
+        .toLowerCase()
+        .includes(search.toLowerCase())
+    );
+  });
+
+  return {
+    search,
+    setSearch,
+    filteredData,
+  };
+};
+
+export default useSearch;
