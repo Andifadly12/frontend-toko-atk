38e0a1a718898f3d138df51de43ff03bc386a931
Andifadly12 <andifadly2004@gmail.com>
2026-07-10 21:03:12 +0800

    keenam pembaharuan: mebuat customersData.js

---
+++ src/data/customersData.js
@@ -0,0 +1,31 @@
+const customersData = [
+  {
+    id: 1,
+    name: "Andi Fadly",
+    phone: "081234567890",
+    email: "andi@gmail.com",
+    address: "Bulukumba",
+    customer_type: "retail",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Toko Berkah",
+    phone: "082345678901",
+    email: "tokoberkah@gmail.com",
+    address: "Makassar",
+    customer_type: "reseller",
+    status: "active",
+  },
+  {
+    id: 3,
+    name: "Siti Aminah",
+    phone: "083456789012",
+    email: "siti@gmail.com",
+    address: "Gowa",
+    customer_type: "retail",
+    status: "inactive",
+  },
+];
+
+export default customersData;
