bf474baa3c8354b7c7cd7a8a1d20ae3f2fad2366
Andifadly12 <andifadly2004@gmail.com>
2026-07-11 14:59:06 +0800

    ketujuh pembaharuan: membuat suppliersData

---
+++ src/data/suppliersData.js
@@ -0,0 +1,31 @@
+const suppliersData = [
+  {
+    id: 1,
+    name: "PT Sumber ATK",
+    contact_person: "Bapak Ahmad",
+    phone: "081234567890",
+    email: "sumberatk@gmail.com",
+    address: "Makassar",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "CV Berkah Jaya",
+    contact_person: "Ibu Sari",
+    phone: "082345678901",
+    email: "berkahjaya@gmail.com",
+    address: "Bulukumba",
+    status: "active",
+  },
+  {
+    id: 3,
+    name: "UD Cahaya Office",
+    contact_person: "Bapak Rudi",
+    phone: "083456789012",
+    email: "cahayaoffice@gmail.com",
+    address: "Gowa",
+    status: "inactive",
+  },
+];
+
+export default suppliersData;
