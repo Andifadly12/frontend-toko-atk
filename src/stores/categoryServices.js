095bf7782554cc33aa251b1caf4edd69660cf095
Andifadly12 <andifadly2004@gmail.com>
2026-07-15 14:50:10 +0800

    kesembulan pembaharuan: membaut CRUD

--- src/stores/categoryServices.js
+++ src/stores/categoryServices.js
@@ -1,4 +1,4 @@
-import api from "../config/api.js";
+import api from "../services/api";
 
 const handleApiError = (error, defaultMessage) => {
   console.log("API ERROR:", error);
@@ -14,11 +14,44 @@ const handleApiError = (error, defaultMessage) => {
 
 export const getCategories = async () => {
   try {
-    const response = await api.get("/categorys");
+    const response = await api.get("/categories");
     return response.data;
   } catch (error) {
-    handleApiError(error, "Gagal mengambil category");
+    handleApiError(error, "Gagal mengambil data kategori");
   } finally {
-    console.log("Get category selesai");
+    console.log("GET categories selesai");
+  }
+};
+
+export const createCategory = async categoryData => {
+  try {
+    const response = await api.post("/categories", categoryData);
+    return response.data;
+  } catch (error) {
+    handleApiError(error, "Gagal menambah kategori");
+  } finally {
+    console.log("CREATE category selesai");
+  }
+};
+
+export const updateCategory = async (id, categoryData) => {
+  try {
+    const response = await api.put(`/categories/${id}`, categoryData);
+    return response.data;
+  } catch (error) {
+    handleApiError(error, "Gagal update kategori");
+  } finally {
+    console.log("UPDATE category selesai");
+  }
+};
+
+export const deleteCategory = async id => {
+  try {
+    const response = await api.delete(`/categories/${id}`);
+    return response.data;
+  } catch (error) {
+    handleApiError(error, "Gagal hapus kategori");
+  } finally {
+    console.log("DELETE category selesai");
   }
 };
