193a1227d78fbf5d8a5785127bbbabb2243df743
Andifadly12 <andifadly2004@gmail.com>
2026-07-18 14:44:01 +0800

    ketigabelas pembaharuan: membuat halaman useLogout

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router";
+import useAuthStore from "../routes/authStore";
+
+const useLogout = () => {
+  const navigate = useNavigate();
+  const logout = useAuthStore(state => state.logout);
+
+  const handleLogout = () => {
+    const confirmLogout = window.confirm("Yakin ingin logout?");
+
+    if (!confirmLogout) return;
+
+    logout();
+    navigate("/login", { replace: true });
+  };
+
+  return handleLogout;
+};
+
+export default useLogout;
